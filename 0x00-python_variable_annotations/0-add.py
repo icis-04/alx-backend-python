326116c761fdcecf45ace613d09e9733ed7a3b68
icis-04 <chidexiwuaku@gmail.com>
2023-01-05 14:28:33 +0000

    first file added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python3
+"""
+File is used to define a type-annotated function
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    add function that defines its variables
+    """
+    return a + b
