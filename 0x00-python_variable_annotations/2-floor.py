a82209ad69c9ed4bf58963cf0049d55c6b060e7c
icis-04 <chidexiwuaku@gmail.com>
2023-01-06 03:27:30 +0000

    floor printing

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+function that prints the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function that returns the floor of a float
+    """
+    return int(n)
