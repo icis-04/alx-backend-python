329e74e029c99dbcf24ffecc5e023abbb9e65bbf
icis-04 <chidexiwuaku@gmail.com>
2023-01-06 04:09:31 +0000

    sum of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+prints the sum of a list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Returns sum of floats
+    """
+    return sum(input_list)
