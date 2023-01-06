ed1eb8b0cb20647584fb3c059373d69388cac733
icis-04 <chidexiwuaku@gmail.com>
2023-01-06 04:41:29 +0000

    problems

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,7 +5,5 @@ File is used to define a type-annotated function
 
 
 def add(a: float, b: float) -> float:
-    """
-    add function that defines its variables
-    """
+    """add function that defines its variables"""
     return a + b
