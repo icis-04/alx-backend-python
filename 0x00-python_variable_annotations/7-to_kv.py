ab6c9483cbb282a8be92ffba427c9312acb4c2b1
icis-04 <chidexiwuaku@gmail.com>
2023-01-07 21:29:32 +0000

    tuples output implemented

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+List that prints Tuple of sy=ting and floats
+"""
+from typing import Union, List, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    return (k, v ** 2)
