9b80ec07b1c5212786b5c971973564f0855419b4
icis-04 <chidexiwuaku@gmail.com>
2023-01-09 23:21:12 +0000

    updated a file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ Script to print out the random wait floats in a list
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random: Callable[[int], float ] = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
