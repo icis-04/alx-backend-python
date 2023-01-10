6ef97192d453359ddbebe153217be24ef81dfddf
icis-04 <chidexiwuaku@gmail.com>
2023-01-10 04:38:21 +0000

    last file added

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,9 +6,8 @@ from typing import List
 import time
 import asyncio
 
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
-
-
 def measure_time(n: int, max_delay: int) -> float:
     """
     Returns the time it takes for an async function to run
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python3
+#!/ur/bin/env python3
 """
-Script to print out the random wait floats in a list
+script designed to wait for a task
 """
-from typing import List, Callable
+from typing import List
 
 
-wait_random: Callable[[int], float] = __import__('0-basic_async_syntax').wait_random
-async def wait_n(n: int, max_delay: int) -> List[float]:
+task_wait_random = __import__('3-tasks').task_wait_random
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     asynchronous function that returns list of floats of awaited time of the 
-    wait_random function.
+    task_wait_random function.
     """
     i: int = 0
     res: List[float] = []
     while i < n:
-        j = await wait_random(max_delay)
+        j = await task_wait_random(max_delay)
         res.append(j)
         i += 1
     for i in range(len(res)):
