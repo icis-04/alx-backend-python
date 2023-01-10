878edadce5de056e4cf24d695544109b9f8f5aaf
icis-04 <chidexiwuaku@gmail.com>
2023-01-10 04:58:26 +0000

    final final commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,7 @@
 """
 Script to print out the random wait floats in a list
 """
-from typing import List, Callable
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
