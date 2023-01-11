a8ec90237955c118b0264d53102a88e84b837f7d
icis-04 <chidexiwuaku@gmail.com>
2023-01-11 00:33:33 +0000

    second commit

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Scripts returns list of floats from the async_generator module using
+asyn list comprehension
+"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Returns a list of floats gotten from the async_generator module
+    """
+    result = [i async for i in async_generator()]
+    return result 
