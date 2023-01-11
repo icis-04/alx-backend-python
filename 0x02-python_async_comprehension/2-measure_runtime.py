a45a64b372154d85a407a59f87e2a6bfca6ad0f4
icis-04 <chidexiwuaku@gmail.com>
2023-01-11 00:47:06 +0000

    third commit

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+"""
+script that calculates the time it takes for an async function to run
+"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Returns the time it takes for an async function to run
+    """
+    start = time.perf_counter()
+    j = asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await j
+    end = time.perf_counter() - start
+    return end
